f0e6aaa9d093c210628d77f58a0f41eae27586de
Fille@pi3 <filipengstrom91@gmail.com>
2024-07-01 10:34:48 +0200

    added support for multiple recipients, added a specific admin email variable in env, added a reset function for the EMAIL_SENT variable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests as rq
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
+from pathlib import Path
 import os
 import smtplib
 import sys
@@ -11,10 +12,13 @@ class FPL:
     def __init__(self):
         self.response_raw = rq.get(FPL.URL)
         load_dotenv()
+        self.path = Path('.env')
 
         self.email_sent = os.getenv('EMAIL_SENT')
         self.app_pw = os.getenv('APP_PW')
-        self.email = os.getenv('EMAIL')
+        self.email_from = os.getenv('EMAIL_FROM')
+        self.email_to = os.getenv('EMAIL_TO')
+        self.email_admin = os.getenv('EMAIL_ADMIN')
 
         if self.response_raw.status_code != 200:
             self.send_email('error', str(self.response_raw.status_code) + ";" + self.response_raw.text)
@@ -28,35 +32,46 @@ class FPL:
     def send_email(self, setting: str = 'normal', error_message: str = ''):
         if setting == 'normal':
             message = 'Subject: {}\n\n{}'.format("FPL REGISTRATION IS OPEN", "GO GET THEM")
+            email_from = self.email_from
+            email_to = self.email_to
         elif setting == 'ping':
             message = 'Subject: {}\n\n{}'.format("PING FPL", "Script is working")
+            email_from = self.email_from
+            email_to = self.email_admin
         else:
             message = 'Subject: {}\n\n{}'.format("ERROR FPL", error_message)
+            email_from = self.email_from
+            email_to = self.email_admin
 
-        try:
-            s = smtplib.SMTP('smtp.gmail.com', 587)
-            s.starttls()
-            s.login(self.email, self.app_pw)
-            s.sendmail(self.email, self.email, message)
-            s.quit()
+        recipients = email_to.split(',')
 
-            return True
-
-        except Exception as e:
-            print(e)
-
-            return False
+        for recipient in recipients:
+            try:
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.starttls()
+                s.login(self.email_from, self.app_pw)
+                s.sendmail(email_from, recipient, message)
+                s.quit()
+            except Exception as e:
+                print(e)
+                return False
+        
+        return True
 
     def run(self):
-        if 0 < self.tot_players < 1_000_000:
-            if self.email_sent is None:
+        if 0 < self.tot_players < 15_000_000:
+            if self.email_sent == 'false':
                 if self.send_email():
-                    with open(".env", "a") as f:
-                        f.write("EMAIL_SENT=1")
+                    set_key(dotenv_path=self.path, key_to_set='EMAIL_SENT', value_to_set='true')
 
     def ping(self):
         if self.send_email('ping'):
             pass
+
+    def reset(self):
+        set_key(dotenv_path=self.path, key_to_set='EMAIL_SENT', value_to_set='false')
+        print('env variable EMAIL_SENT reset to false')
+    
 
 
 if __name__ == '__main__':
@@ -65,6 +80,10 @@ if __name__ == '__main__':
     if not fpl.shit_hit_the_fan:
         if len(sys.argv) == 1:
             fpl.run()
+            sys.exit()
 
-        else:
+        if sys.argv[1] == '--ping':
             fpl.ping()
+
+        if sys.argv[1] == '--reset':
+            fpl.reset()
